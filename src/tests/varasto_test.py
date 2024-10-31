3ce93a57be0b28a2143012d959278563d20a9a86
jasminlei <jasminleinonen@hotmail.com>
2024-10-31 19:33:13 +0200

    add tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,32 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_lisataan_liikaa_tavaraa(self):
+        self.varasto.lisaa_varastoon(12)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_otetaan_enemman_kuin_saldo(self):
+        self.varasto.ota_varastosta(2)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_otetaan_alle_nolla(self):
+        self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisataan_alle_nolla(self):
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_uudella_varastolla_negatiivinen_tilavuus(self):
+        negatiivinen_varasto = Varasto(-10)
+        self.assertAlmostEqual(negatiivinen_varasto.tilavuus, 0)
+
+    def test_uudella_varastolla_negatiivinen_saldo(self):
+        negatiivinen_saldo = Varasto(10, -5)
+        self.assertAlmostEqual(negatiivinen_saldo.saldo, 0)
+
+    def test_str_palauttaa_oikein(self):
+        self.varasto.lisaa_varastoon(5)
+        odotettu_arvo = "saldo = 5, vielä tilaa 5"
+        self.assertEqual(str(self.varasto), odotettu_arvo)
